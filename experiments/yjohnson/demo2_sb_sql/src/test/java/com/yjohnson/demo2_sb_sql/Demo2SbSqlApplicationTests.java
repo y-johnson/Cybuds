d2f43a1e77bd4a843b7c65ca2b075479cfc3f77c
Yadiel Johnson <yjohnson@iastate.edu>
2021-09-22 14:08:47 -0500

    SQL Testing

---
+++ experiments/yjohnson/demo2_sb_sql/src/test/java/com/yjohnson/demo2_sb_sql/Demo2SbSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjohnson.demo2_sb_sql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2SbSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
