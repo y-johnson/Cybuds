57c40e5e0765c70a0ee234b665ba36022156a8a2
Nick Bergan <njbergan@iastate.edu>
2021-10-06 00:55:43 -0500

    Registration Merge Started
    Work in Progress.

--- frontend/androidApp/app/src/main/java/edu/coms309/cybuds/MainActivity.java
+++ frontend/androidApp/app/src/main/java/edu/coms309/cybuds/MainActivity.java
@@ -6,13 +6,33 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import static edu.coms309.cybuds.api.ApiClientFactory.GetUserApi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import edu.coms309.cybuds.api.SlimCallback;
+import edu.coms309.cybuds.model.User;
+
+import java.util.List;
+//import test.connect.myapplication.api.SlimCallback;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
 
     public void btnLogin_onClick(View view) {
         Intent gotoLoginActivityIntent = new Intent(this, Login.class);
