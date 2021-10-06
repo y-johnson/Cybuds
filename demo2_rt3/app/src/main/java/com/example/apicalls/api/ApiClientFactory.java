2434a831364299f2a8b3bf8452da3e545fa80ac7
CYHUB23 <jharvey1@iastate.edu>
2021-10-05 22:44:18 -0500

    import outside files into git, git issue #6

---
+++ demo2_rt3/app/src/main/java/com/example/apicalls/api/ApiClientFactory.java
@@ -0,0 +1,26 @@
+package com.example.apicalls.api;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClientFactory {
+
+    static Retrofit apiClientSeed = null;
+
+    static Retrofit GetApiClientSeed() {
+
+        if (apiClientSeed == null) {
+
+            Retrofit retrofit = new Retrofit.Builder() //http://10.0.2.2:8080/
+                    .baseUrl("http://coms-309-028.cs.iastate.edu:8080/") //get our server url
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build(); //http://coms-309-028.cs.iastate.edu:8080
+        }
+        return apiClientSeed;
+    }
+
+    public static UserApi GetUserApi(){
+        return GetApiClientSeed().create(UserApi.class);
+    }
+
+}
