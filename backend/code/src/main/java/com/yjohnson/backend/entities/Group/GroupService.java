b0fc641074601a9b26f3546a9d025ceab5302587
Yadiel Johnson <yjohnson@iastate.edu>
2021-12-10 01:31:06 -0600

    Group loading

--- backend/code/src/main/java/com/yjohnson/backend/entities/Group/GroupService.java
+++ backend/code/src/main/java/com/yjohnson/backend/entities/Group/GroupService.java
@@ -1,16 +1,55 @@
 package com.yjohnson.backend.entities.Group;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 @Service
 public class GroupService {
+	public static final String STATIC_MAJORS_TXT = "/majors.txt";
+	public static final String STATIC_COLLEGES_TXT = "/colleges.txt";
+	private static boolean loaded = false;
 	private final GroupRepository groupRepository;
 
 	public GroupService(GroupRepository groupRepository) {
 		this.groupRepository = groupRepository;
+		preloadGroups(groupRepository, "%s is a major at ISU.", GroupType.STUDENT_MAJOR, STATIC_MAJORS_TXT);
+		preloadGroups(groupRepository, "%s is a college at ISU.", GroupType.COLLEGE, STATIC_COLLEGES_TXT);
+	}
+
+	private static void preloadGroups(GroupRepository groupRepository, String format, GroupType groupType, String file) {
+		if (!loaded){
+			URL url = GroupService.class.getResource(file);
+			assert url != null;
+			try (Stream<String> stream = Files.lines(Paths.get(url.toURI()))) {
+				stream.forEach((name) -> {
+					if (!groupRepository.findByName(name).isPresent()) {
+						try {
+							groupRepository.save(new GroupEntity(
+									groupType,
+									name,
+									String.format(format, name)
+							));
+						} catch (DataIntegrityViolationException ignored) {
+							System.err.println("Save failed for " + name);
+						}
+					}
+				});
+			} catch (IOException | URISyntaxException | NullPointerException e) {
+				e.printStackTrace();
+			} finally {
+				loaded = true;
+			}
+		}
+
 	}
 
 	protected Optional<GroupEntity> getGroupByID(Long id) {
@@ -58,7 +97,7 @@ public class GroupService {
 		}
 	}
 
-	protected Iterable<GroupEntity> getGroupsByType(GroupType type){
+	protected Iterable<GroupEntity> getGroupsByType(GroupType type) {
 		return groupRepository.findAllByGroupType(type);
 	}
 }
