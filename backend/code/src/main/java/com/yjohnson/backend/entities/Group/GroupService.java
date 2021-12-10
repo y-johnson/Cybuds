d154212ee20755cace4f33b72ccda655e729eb11
Yadiel Johnson <yjohnson@iastate.edu>
2021-12-10 02:19:26 -0600

    Fix for groups

--- backend/code/src/main/java/com/yjohnson/backend/entities/Group/GroupService.java
+++ backend/code/src/main/java/com/yjohnson/backend/entities/Group/GroupService.java
@@ -1,16 +1,74 @@
 package com.yjohnson.backend.entities.Group;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
+import org.yaml.snakeyaml.constructor.SafeConstructor;
 
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Optional;
+import java.util.Scanner;
+import java.util.stream.Stream;
 
 @Service
 public class GroupService {
+	public static final String STATIC_MAJORS_TXT = "/static/txt/majors.txt";
+	public static final String STATIC_COLLEGES_TXT = "/static/txt/colleges.txt";
+	private static boolean loaded = false;
 	private final GroupRepository groupRepository;
+	@Value("classpath:static/txt/*")
+	private Resource[] resources;
+	private List<String> filenames;
 
 	public GroupService(GroupRepository groupRepository) {
 		this.groupRepository = groupRepository;
+
+	}
+
+	@PostConstruct
+	void init() throws IOException {
+		InputStream f = new ClassPathResource("static/txt/majors.txt").getInputStream();
+		preloadGroups(groupRepository, "%s is a major at ISU.", GroupType.STUDENT_MAJOR, f);
+
+		f = new ClassPathResource("static/txt/colleges.txt").getInputStream();
+
+		preloadGroups(groupRepository, "%s is a college at ISU.", GroupType.COLLEGE, f);
+	}
+
+	private static void preloadGroups(GroupRepository groupRepository, String format, GroupType groupType, InputStream file) {
+		if (!loaded) {
+
+			try (Scanner scanner = new Scanner(file).useDelimiter("\\r?\\n")) {
+				while (scanner.hasNext()){
+					String name = scanner.next();
+					if (!groupRepository.findByName(name).isPresent()) {
+						try {
+							groupRepository.save(new GroupEntity(
+									groupType,
+									name,
+									String.format(format, name)
+							));
+						} catch (DataIntegrityViolationException ignored) {
+							System.err.println("Save failed for " + name);
+						}
+					}
+				}
+			} finally {
+				loaded = true;
+			}
+		}
+
 	}
 
 	protected Optional<GroupEntity> getGroupByID(Long id) {
@@ -58,7 +116,7 @@ public class GroupService {
 		}
 	}
 
-	protected Iterable<GroupEntity> getGroupsByType(GroupType type){
+	protected Iterable<GroupEntity> getGroupsByType(GroupType type) {
 		return groupRepository.findAllByGroupType(type);
 	}
 }
